0209d245ba05df1ac34be02a87245a3f181060c0
Your Name <you@example.com>
2018-11-02 19:47:53 +0300

    overflow  'hidden'

--- js/index.js
+++ js/index.js
@@ -11,14 +11,18 @@ window.addEventListener('DOMContentLoaded', function () {
   popupClose.addEventListener('click', function (e) {
     e.preventDefault();
     popupEngineer.style.display = 'none';
+    document.body.style.overflow = '';
     
   });
   popup.addEventListener('click', function (e) {
     e.preventDefault();
     popup.style.display = 'none';
+    document.body.style.overflow = '';
+
 
   });
-      
+
+ 
   body.addEventListener('click', function (e) {
     let target = e.target;
     e.preventDefault();
@@ -26,26 +30,27 @@ window.addEventListener('DOMContentLoaded', function () {
     if (target.classList.contains('popup_engineer_btn')) {
       
       popupEngineer.style.display = 'block';
+      document.body.style.overflow = 'hidden';
 
     }
     if (target.classList.contains('popup_engineer')) {
       popupEngineer.style.display = 'none';
-     
+      document.body.style.overflow = '';
+      console.log('sd');
     }
    // popup
     
     if (target.classList.contains('phone_link')) {
-
+      document.body.style.overflow = 'hidden';
       popup.style.display = 'block';
 
     }
     if (target.classList.contains('popup_close')) {
       popup.style.display = 'none';
+      document.body.style.overflow = '';
 
     }
-    if (target.classList.contains('phone_link')) {
-      
-    }
+    
   });
     
  
